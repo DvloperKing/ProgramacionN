c0311886c5a27301e0874cc14396c60050924192
consmor7 <69411099+consmor7@users.noreply.github.com>
2025-03-30 20:09:47 -0600

    Ingreso de una fecha y mensaje si corresponde al primer trimestre.

---
+++ Desktop/Programacion Nativa/CursoKt/Ejemplos/src/proyecto13.kt
@@ -0,0 +1,14 @@
+//Se carga una fecha (día, mes y año) por teclado. Mostrar un mensaje si corresponde al
+//primer trimestre del año (enero, febrero o marzo)
+//Cargar por teclado el valor numérico del día, mes y año. Ejemplo: dia:10 mes:2 año:2017.
+
+fun main(parametro: Array<String>) {
+    print("Ingrese día:")
+    val dia = readLine()!!.toInt()
+    print("Ingrese mes:")
+    val mes = readLine()!!.toInt()
+    print("Ingrese Año:")
+    val año = readLine()!!.toInt()
+    if (mes == 1 || mes == 2 || mes == 3)
+        print("Corresponde al primer trimestre");
+}
