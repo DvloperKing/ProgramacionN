0114a62bed09b7382b1e120b27b77dd2324eeedf
consmor7 <69411099+consmor7@users.noreply.github.com>
2025-03-30 19:48:53 -0600

    proyecto9-Obtener el mayor entre dos números usando if como expresión.

---
+++ Desktop/Programacion Nativa/CursoKt/Ejemplos/src/proyecto9.kt
@@ -0,0 +1,9 @@
+//Cargar dos valores enteros, almacenar el mayor de los mismos en otra variable y mostrarlo.
+fun main(parametro: Array<String>) {
+    print("Ingrese primer valor:")
+    val valor1 = readLine()!!.toInt()
+    print("Ingrese segundo valor:")
+    val valor2 = readLine()!!.toInt()
+    val mayor = if (valor1 > valor2) valor1 else valor2
+    println("El mayor entre $valor1 y $valor2 es $mayor")
+}
