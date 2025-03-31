4bdbe9a234f6eb0835e97bb5606aad35879b501c
consmor7 <69411099+consmor7@users.noreply.github.com>
2025-03-30 20:02:32 -0600

    Proyecto12 - Ingreso de tres números y mostrar el mayor usando operadores lógicos.

---
+++ Desktop/Programacion Nativa/CursoKt/Ejemplos/src/proyecto12.kt
@@ -0,0 +1,16 @@
+//Ingreso de tres números y mostrar el mayor usando operadores lógicos.
+fun main(parametro: Array<String>) {
+    print("Ingrese primer valor:")
+    val num1 = readLine()!!.toInt()
+    print("Ingrese segundo valor:")
+    val num2 = readLine()!!.toInt()
+    print("Ingrese tercer valor:")
+    val num3 = readLine()!!.toInt()
+    if (num1 > num2 && num1 > num3)
+        print(num1)
+    else
+        if (num2 > num3)
+            print(num2)
+        else
+            print(num3);
+}
